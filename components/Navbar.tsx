aa73d5103db66339138b18b51579310d30a140d2
Street Issue <streetissue@yahoo.com>
2026-07-09 22:24:10 +0100

    Fixed navbar links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,38 +1,38 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-black/70 backdrop-blur-lg border-b border-white/10">
-
       <div className="max-w-7xl mx-auto flex items-center justify-between px-8 py-5">
 
-        <a
+        <Link
           href="/"
           className="text-3xl font-black tracking-[0.25em] hover:opacity-80 duration-300"
         >
           SI
-        </a>
+        </Link>
 
         <div className="flex items-center gap-10 uppercase tracking-[0.2em] text-sm">
 
-          <a href="/" className="hover:text-gray-400 duration-300">
+          <Link href="/" className="hover:text-gray-400 duration-300">
             Home
-          </a>
+          </Link>
 
-          <a href="#" className="hover:text-gray-400 duration-300">
+          <Link href="/shop" className="hover:text-gray-400 duration-300">
             Shop
-          </a>
+          </Link>
 
-          <a href="#" className="hover:text-gray-400 duration-300">
+          <Link href="/#about" className="hover:text-gray-400 duration-300">
             About
-          </a>
+          </Link>
 
-          <a href="#" className="hover:text-gray-400 duration-300">
+          <Link href="/#contact" className="hover:text-gray-400 duration-300">
             Contact
-          </a>
+          </Link>
 
         </div>
 
       </div>
-
     </nav>
   );
 }
